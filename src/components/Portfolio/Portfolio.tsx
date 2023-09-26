d2c05634bf079e4aa2693e85c3b500e2e9715e85
Priceless001 <ayenpre@gmail.com>
2023-09-26 20:44:47 +0100

    initial commit

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -129,14 +129,14 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="#66bfff" stroke="#66bfff " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://codewunder.vercel.app/" target="_blank" rel="noreferrer">
+              <a href="https://brilliant-conkies-1e083b.netlify.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visit Site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Personal Portfolio</h3>
-              <p>A blend of TypeScript and React. Experience seamless scrolling and captivating animations. Let's connect through my projects and journey!         
+              <h3>An Image Gallery</h3>
+              <p>A blend of HTML,CSS and React. Experience seamless scrolling and Login. Let's connect through and journey!         
               </p>
             </div>
             <footer>
